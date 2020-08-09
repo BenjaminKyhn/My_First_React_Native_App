2d02a7680c72d954866e88b08bc7cd7a7b24f9f8
Benjamin Kyhn <benkyhn@hotmail.com>
2020-08-09 14:05:56 +0200

    Part 4.5 complete

---
+++ Tutorial_04.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import {View, StyleSheet} from "react-native";
+import ColorButton from "./components/ColorButton"
+
+export default function App() {
+    const [backgroundColor, setBackgroundColor] = useState("blue");
+    return (
+        <View style={[styles.container, {backgroundColor}]}>
+            <ColorButton backgroundColor="red" onPress={setBackgroundColor}/>
+            <ColorButton backgroundColor="purple" onPress={setBackgroundColor}/>
+            <ColorButton backgroundColor="blue" onPress={setBackgroundColor}/>
+            <ColorButton backgroundColor="green" onPress={setBackgroundColor}/>
+            <ColorButton backgroundColor="yellow" onPress={setBackgroundColor}/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+});
